bef50adca1f618d8760c94fe3b5d81b2e4188875
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2022-03-16 14:54:57 +0700

    Update PB2_ICE-Menu_v1.0.lua
    Remember the update v2.0.8 that deprecates ICE menu? yes. as a result, i update this to v1.1:
    + Based on the last cheat on Pb2Chts before its removed.
    + Much cleaner code.
    + Unnecesary stuff removed (except the emoji thing, because i want to keep things as close to original as possible).
    making use of loopSearch().
    + remove exit message nonsense.
    + now the script wont quit if os.quit isnt executed.

--- script_collections/PB2_ICE-Menu_v1.0.lua
+++ script_collections/PB2_ICE-Menu_v1.0.lua
@@ -1,128 +1,127 @@
---main code
---Coder: TOXIC MODS
---Version: 1.0
---Game: Payback 2
---Desc: Payback 2 ice menu
+--[[
+	Coder: CrystalMods (uploader: TOXIC MODS)
+	Version: 1.1
+	Game: Payback 2
+	Desc: Payback 2 ICE menu
+]]
+function Menu()
+	CH = gg.choice({
+		"📀Speed Menu v1.0📀",
+		"😍Give Grenades😍",
+		"🙃Give C4s🙃",
+		"🛡Give Laser🛡",
+		"😂Win Level😂",
+		"📛Exit📛"
+	},nil,"🛡😜ICE MENU v1.1😜🛡")
+	if CH==1 then spd() end
+	if CH==2 then rl() end
+	if CH==3 then c4() end
+	if CH==4 then ls() end
+	if CH==5 then win() end
+	if CH==6 then os.exit() end
+end
 function spd()
-myMenu = gg.choice({
+CH = gg.choice({
 '☺Speed x50☺',
 '☺Speed x100☺',
 '☺Speed x500☺',
 '😐Exit😐'}
 ,nil,"😔Speed Menu v1.0😌")
-if myMenu==1 then
+if CH==1 then
 gg.setSpeed(50)
 end
-if myMenu==2 then
+if CH==2 then
 gg.setSpeed(100)
 end
-if myMenu==3 then
+if CH==3 then
 gg.setSpeed(500)
 end
-if myMenu==4 then
-gg.alert('Script coded by: TOXIC MODS')
-os.exit()
-end
-if myMenu==nil then end
 end
 function rl()
-local gg = gg
-gg.setRanges(gg.REGION_C_BSS)
-gg.searchNumber("20", gg.TYPE_DWORD)
-gg.alert('Change Ammo to: 16, 10 seconds')
-gg.sleep(10000)
-gg.refineNumber("16", gg.TYPE_DWORD)
-x = gg.getResults(1)
-local y = {}
-y[1] = {}
-y[1].address = x[1].address + 0x4
-y[1].flags = gg.TYPE_FLOAT
-y[1].value = 1
-gg.setValues(y)
-gg.clearResults()
-gg.toast('Grenades: ON')
+	gg.setRanges(gg.REGION_C_BSS)
+	t = loopSearch(1,gg.TYPE_DWORD,'Put your grenade ammo')
+	if gg.getResultCount() == 0 then
+		gg.toast("Can't find the ammo")
+	else
+		gg.setValues({{
+			address = t[1].address + 0x4,
+			flags = gg.TYPE_FLOAT,
+			value = 1
+		}})
+		gg.clearResults()
+		gg.toast('Grenade ammo ON')
+	end
 end
-function cFour()
-local gg = gg
-gg.setRanges(gg.REGION_C_BSS)
-gg.searchNumber("16", gg.TYPE_DWORD)
-gg.alert('Change value to: 10, 10 seconds')
-gg.sleep(10000)
-gg.refineNumber("10", gg.TYPE_DWORD)
-e = gg.getResults(1)
-local v = {}
-v[1] = {}
-v[1].address = e[1].address + 0x8
-v[1].flags = gg.TYPE_FLOAT
-v[1].value = 1
-gg.setValues(v)
-gg.clearResults()
-gg.toast('C4: ON')
+function c4()
+	gg.setRanges(gg.REGION_C_BSS)
+	t = loopSearch(1,gg.TYPE_DWORD,'Put your sticky bomb ammo')
+	if gg.getResultCount() == 0 then
+		gg.toast("Can't find the ammo")
+	else
+		gg.setValues({{
+			address = t[1].address + 0x8,
+			flags = gg.TYPE_FLOAT,
+			value = 1
+		}})
+		gg.clearResults()
+		gg.toast('C4 ammo ON')
+	end
 end
 function ls()
-local gg = gg 
-gg.setRanges(gg.REGION_C_BSS)
-gg.searchNumber("10", gg.TYPE_DWORD)
-gg.alert('Change value to: 6, 10 seconds')
-gg.sleep(10000)
-gg.refineNumber("6", gg.TYPE_DWORD)
-owo = gg.getResults(1)
-local uwu = {}
-uwu[1] = {}
-uwu[1].address = owo[1].address + 0xC
-uwu[1].flags = gg.TYPE_FLOAT
-uwu[1].value = 1
-gg.setValues(uwu)
-gg.clearResults()
-gg.toast('Laser: ON')
+	gg.setRanges(gg.REGION_C_BSS)
+	t = loopSearch(1,gg.TYPE_DWORD,'Put your laser ammo')
+	if gg.getResultCount() == 0 then
+		gg.toast("Can't find the ammo")
+	else
+		gg.setValues({{
+			address = t[1].address + 0xC,
+			flags = gg.TYPE_FLOAT,
+			value = 1
+		}})
+		gg.clearResults()
+		gg.toast('Laser ammo ON')
+	end
+	tmp = nil
 end
 function win()
-local gg = gg
-gg.setRanges(gg.REGION_ANONYMOUS)
-stpOne = gg.prompt({'ENTER AMMO'},{[1] = '0'},{[1] = 'number'})
-gg.searchNumber(stpOne[1], gg.TYPE_DWORD)
-gg.alert('10 seconds to change value')
-gg.sleep(10000)
-stpTwo = gg.prompt({'ENTER NEW VALUE'},{[1] = '0'},{[1] = 'number'})
-sop = gg.refineNumber(stpTwo[1], gg.TYPE_DWORD)
-local e = {}
-e[1] = {}
-e[1].address = sop[1].address + 0x14
-e[1].flags = gg.TYPE_FLOAT
-e[1].value = 1
-gg.setValues(e)
-gg.clearResults()
-gg.toast('Level Won:ON')
+	gg.setRanges(gg.REGION_ANONYMOUS)
+	t = loopSearch(1,gg.TYPE_DWORD,'Put one of your weapon ammo (the original ICE Menu dev told that all ammo can work, this might wrong)')
+	if gg.getResultCount() == 0 then
+		gg.toast("Can't find the ammo")
+	else
+		gg.setValues({{
+			address = t[1].address + 0x14,
+			flags = gg.TYPE_FLOAT,
+			value = 1
+		}})
+		gg.clearResults()
+		gg.toast('Level Win ON')
+	end
 end
-function stop()
-gg.alert('Thank you for using this script: please sub to TOXIC MODS')
-gg.toast('Script by: TOXIC MODS')
-os.exit()
+
+function loopSearch(dsrdRsltCnt,valType,msg1)
+	local num1,t = gg.prompt({msg1})
+	if (num1 and num1[1]) then
+		gg.searchNumber(num1[1],valType,nil,nil)
+		if gg.getResultCount() > 0 then
+			while gg.getResultCount() > dsrdRsltCnt do
+				tmp = gg.getResultCount()
+				gg.toast('3 seconds to change ammo value')
+				gg.sleep(3e3)
+				num1 = gg.prompt({'Put your weapon ammo\nfound '..tmp},{num1[1]})
+				if not (num1 and num1[1]) then break end
+				gg.refineNumber("0~32767")
+				gg.refineNumber(num1[1])
+				tmp = gg.getResultCount()
+				return gg.getResults(dsrdRsltCnt)
+			end
+		end
+	end
 end
-menu = gg.choice({
-  "📀Speed Menu v1.0📀",
-  "😍Give Grenades😍",
-  "🙃Give C4s🙃",
-  "🛡Give Laser🛡",
-  "😂Win Level😂",
-  "📛Exit📛"
-  },nil,"🛡😜ICE MENU v1.0😜🛡")
-  if menu==1 then
-  spd()
-  end
-  if menu==2 then
-  rl()
-  end
-  if menu==3 then
-  cFour()
-  end
-  if menu==4 then
-  ls()
-  end
-  if menu==5 then
-  win()
-  end
-  if menu==5 then
-  stop()
-  end
-  if menu==nil then end
+
+while true do
+	if gg.isVisible() then
+		gg.setVisible(false)
+		Menu()
+	end
+end
