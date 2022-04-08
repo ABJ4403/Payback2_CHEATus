5631c4c3794c4b4a779d67374388c7a1a5390040
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2022-04-09 00:06:24 +0700

    Update string_decryptor.lua
    "minify" helper function
    simplify `Decryptor_1()` (might fail because its done in quickdirty way).
    Simplify `Decryptor_2()`
    change dec1b to dec2b because its just mathematically same, but the output is tables.
    Properly implement (or should i say fix) `Encryptor_2()`

--- script_collections/decompiler/string_decryptor.lua
+++ script_collections/decompiler/string_decryptor.lua
@@ -1,112 +1,47 @@
 -- Function helpers
-function tbl2hex(bytes)
-	local r = ''
-	for i, b in ipairs(bytes) do
-		r = r .. string.format('%02X ', b)
-	end
-	return r
-end
-
-function str2hex(s)
-	return tbl2hex(gg.bytes(s, encoding))
-end
-
-function hex2bin(s)
-	return string.char(tonumber(s, 16))
-end
-
-function str2tbl(s)
-	local result = {}
-	s:gsub(".",function(c) table.insert(result,c) end)
-	return result
-end
+function tbl2hex(bytes)local r = '' for i, b in ipairs(bytes) do r = r .. string.format('%02X ',b)end return r end
+function str2hex(s)return tbl2hex(gg.bytes(s, encoding))end
+function hex2bin(s)return string.char(tonumber(s, 16))end
+function str2tbl(s)local result = {} s:gsub(".",function(c)table.insert(result,c)end) return result end
 
 -- Decryptors
-function Decryptor_1(input)
+function Decryptor_1(input) -- uncommon decryptor, found om Hydra wall hack script
 	local result = ""
-	A2_2 = 22
-	A3_2 = 34
-	A4_2 = 43475
 	for i in ipairs(input) do
-		A9_3 = input[i]
-		A10_3 = A2_2
+		rres = input[i]
+		A10_3 = 22
 		if i == 1 then
-			A9_3 = A9_3 + A10_3
-			A10_3 = A3_2
-			A10_3 = A10_3 - 1
-			A11_3 = A2_2
-			A11_3 = A11_3 + 1
-			A10_3 = A10_3 * A11_3
-			A9_3 = A9_3 + A10_3
-			A9_3 = A9_3 % 256
+			rres = rres + 22 + 759 % 256
 		end
 		if i == 2 then
-			A9_3 = A9_3 - A10_3
-			A10_3 = A3_2
-			A10_3 = A10_3 + 2
-			A11_3 = A2_2
-			A11_3 = A11_3 + 2
-			A10_3 = A10_3 * A11_3
-			A9_3 = A9_3 + A10_3
-			A9_3 = A9_3 % 256
+			rres = rres - 22 + 864 % 256
 		end
 		if 2 < i and i < 6 then
-			A11_3 = A3_2
-			A10_3 = A10_3 + A11_3
-			A10_3 = A10_3 + i
-			A10_3 = A10_3 * 4
-			A11_3 = A2_2
-			A11_3 = A11_3 + i
-			A10_3 = A10_3 * A11_3
-			A10_3 = A10_3 % 256
-			A9_3 = A9_3 + A10_3
+			rres = rres + ((56 + i * 4) * (22 + i) % 256)
 		end
 		if 5 < i then
-			A11_3 = A3_2
-			A10_3 = A10_3 + A11_3
-			A10_3 = A10_3 + i
-			A10_3 = A10_3 * 4
-			A11_3 = A2_2
-			A11_3 = A11_3 + i
-			A10_3 = A10_3 * A11_3
-			A10_3 = A10_3 * 124882960
-			A10_3 = A10_3 % 256
-			A11_3 = A4_2
-			A10_3 = A10_3 * A11_3
-			A9_3 = A9_3 + A10_3
+			rres = rres + (((56 + i * 4) * (22 + i) * 124882960 % 256) * 43475)
 		end
-		result = result .. string.char(A9_3)
+		result = result..string.char(rres)
 	end
 	return result
 end
---dec1 helper
-function Decryptor_2(input)
-	input = input.gsub(input, " ", "")
-	function nonsense1(input2)
-		return string.char((tonumber(input2, 16) + 500) % 256)
-	end
-	return input.gsub(input, "..", nonsense1)
+function Decryptor_2(i)
+	i = i.gsub(i," ","") -- optional
+	return i.gsub(i,"..",function(ii)return string.char((tonumber(ii,16) + 500) % 256)end)
 end
-function Decryptor_1b(A0_3)
-	A0_3 = A0_3.gsub(A0_3, " ", "")
-	A0_3 = A1_3
-	A2_3 = A0_3
-	A1_3 = A0_3.gsub
-	A3_3 = ".."
-	function A4_3(A0_4)
-		A1_4 = string.char
-		A2_4 = tonumber
-		A3_4 = A0_4
-		A4_4 = 16
-		A2_4 = A2_4(A3_4, A4_4)
-		A2_4 = A2_4 + 500
-		A2_4 = A2_4 % 256
-		return A1_4(A2_4)
-	end
-	A1_3 = A1_3(A2_3, A3_3, A4_3)
-	return A1_3
+function Decryptor_2b(input) -- same as dec2 but for tables
+  local result = ""
+  for i=0,#input do
+  --convert hex to byte, do some decrypt magic, convert it to char, and concat em
+    result = result..string.char((tonumber(input[i],16) + 500) % 256)
+  end
+  return result
 end
 -- BETA
+function Encryptor_2(input)
+	return table.concat({input:gsub('.',function(c)return string.format('%02X',(string.byte(c) - 500) % 256)end)})
+end
 function Encryptor_3(input)
   local tmp,result = {},{}
 --convert string to bytes
@@ -119,14 +54,7 @@ function Encryptor_3(input)
   end
   return result
 end
-function Decryptor_3(input)
-  local result = ""
-  for i in ipairs(input) do
-  --convert hex to byte, do some decrypt magic, convert it to char, and concat em
-    result = result .. string.char(tonumber(input[i], 16) + 1 % 256)
-  end
-  return result
-end
+
 
 
 
